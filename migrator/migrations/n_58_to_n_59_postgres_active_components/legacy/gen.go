16d6118b7da5f0c6d6807308da3adcd4482f0885
Cong Du <c-du@users.noreply.github.com>
2022-07-01 12:31:53 -0700

    dackbox

---
+++ migrator/migrations/n_58_to_n_59_postgres_active_components/legacy/gen.go
@@ -0,0 +1,4 @@
+package legacy
+
+// //go:generate  cp ../../../../central/activecomponent/datastore/internal/store/postgres/store.go .
+// //go:generate  cp ../../../../central/activecomponent/dackbox/crud.go .
